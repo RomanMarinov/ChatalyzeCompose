c0f48797788f4bd8dfa8b806d61babe2211fcf97
romanmarinov <marinov37@mail.ru>
2023-08-23 23:40:09 +0300

    Initial commit

---
+++ app/src/main/java/com/dev_marinov/chatalyze/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.dev_marinov.chatalyze
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.dev_marinov.chatalyze.ui.theme.ChatalyzeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ChatalyzeTheme {
+             
+            }
+        }
+    }
+}
